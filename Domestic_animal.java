1476bf1b195f43ced47f9be1f35d3ab9956fb1b1
Evgeniia <jonnydor@gmail.com>
2024-08-26 22:33:48 +0300

    Filled domestic animals

--- Domestic_animal.java
+++ Domestic_animal.java
@@ -1,3 +1,14 @@
-public class Domestic_animal {
+import java.time.LocalDate;
+
+public class Domestic_animal extends Animal{
+
+    public Domestic_animal(String name, LocalDate birthDate) {
+        super(name, birthDate);
+    }
     
+    @Override
+    public String toString() {
+        return String.format("Домашнее животное. Имя: %s, Дата рождения: %t", getName(), getBirthDate());
+    }
+
 }
